a78e036910b37b6677d2ab22c5fa3911e536762f
Kathryn Carr and Lizzie Koehler <kathrynelizabethcarr@gmail.com>
2015-03-11 13:48:19 -0700

    Remove add items in startList

--- controllers/trip.js
+++ controllers/trip.js
@@ -3,22 +3,6 @@ BeTogether.TripController = Ember.ObjectController.extend({
   actions: {
     startList: function(){
       this.set("list", true);
-      var newItem1 = {name: "1-person tent"};
-      var newItem2 = {name: "2-person tent"};
-      var newItem3 = {name: "3-person tent"};
-      var newItem4 = {name: "4-person tent"};
-      var newItem5 = {name: "5-person tent"};
-
-      this.get('model').then(function(trip){
-        trip.get("campingList").then(function(campingList) {
-          campingList.get("items").then(function(items){
-            items.pushObject("newItem1");
-            items.save();
-            campingList.save();
-          });
-        });
-      });
-
     }
   }
 });
--- routes/campingList.js
+++ routes/campingList.js
@@ -1,5 +1,5 @@
 BeTogether.CampingListRoute = Ember.Route.extend({
-  model: function() {
-    return this.store.find('campingList');
+  model: function(params) {
+    return this.store.find('campingList', params.campingList_id);
   }
 });

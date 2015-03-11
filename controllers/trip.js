2912eb34f20d70d74549bf2dce6e463f63f4cc5c
Kathryn Carr and Lizzie Koehler <student@epicodus.com>
2015-03-10 17:01:37 -0700

    begin creating a default list

--- controllers/trip.js
+++ controllers/trip.js
@@ -1,8 +1,23 @@
 BeTogether.TripController = Ember.ObjectController.extend({
   list: false,
   actions: {
-    showList: function(){
+    startList: function(){
       this.set("list", true);
+      var newItem1 = {name: "1-person tent"};
+      var newItem2 = {name: "2-person tent"};
+      var newItem3 = {name: "3-person tent"};
+      var newItem4 = {name: "4-person tent"};
+      var newItem5 = {name: "5-person tent"};
+
+    debugger;
+      this.get("campingList").then(function(campingList){
+        campingList.get("items").then(function(items){
+          items.pushObject("newItem1");
+          items.save();
+          campingList.save();
+        });
+      });
+
     }
   }
 });

491caa96b49bc73aa89b567a20dc4bba4d6b27ba
Kathryn Carr and Lizzie Koehler <kathrynelizabethcarr@gmail.com>
2015-03-11 14:55:00 -0700

    Add a stock item to all campingList

--- controllers/trips.js
+++ controllers/trips.js
@@ -5,7 +5,24 @@ BeTogether.TripsController = Ember.Controller.extend({
         name: this.get("tripName")
       });
 
+
+      var newItem = this.store.createRecord('campingListItem', {
+        name: "tent"
+      });
+
+      newItem.set("trip", newTrip);
+
+      newItem.save();
+
+      newTrip.get("campingListItems").then(function(items){
+        items.pushObject(newItem);
+      });
+
       newTrip.save();
+      //
+      // newItem.get('trip').then(function(trip){
+      //   trip.save();
+      // });
 
       this.set("tripName", null);
     }

ab16566c1d2d2b8a3016846ff6858e03da1499da
Kathryn Carr and Lizzie Koehler <lizziefkoehler@gmail.com>
2015-03-12 14:53:18 -0700

    add increment buttons to group list

--- controllers/campingListItem.js
+++ controllers/campingListItem.js
@@ -1,4 +1,5 @@
 BeTogether.CampingListItemController= Ember.ObjectController.extend({
+  amount: 0,
   updateNeedStatus: function(key,value){
       if(value === undefined){
         return this.get("need");
@@ -6,6 +7,16 @@ BeTogether.CampingListItemController= Ember.ObjectController.extend({
         this.set("need",value);
         this.get('model').save();
       }
-  }.property('model.updateNeedStatus')
+  }.property('model.updateNeedStatus'),
+
+  actions: {
+    more: function() {
+      this.incrementProperty('amount');
+    },
+    less: function() {
+      this.decrementProperty('amount');
+    }
+  }
+
 
 });

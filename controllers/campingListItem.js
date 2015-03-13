28759ed3c6add9ef5df318708139ea8c3124ff6a
Kathryn Carr and Lizzie Koehler <kathrynelizabethcarr@gmail.com>
2015-03-13 10:07:54 -0700

    remove checkboxes

--- controllers/campingListItem.js
+++ controllers/campingListItem.js
@@ -1,21 +1,5 @@
 BeTogether.CampingListItemController= Ember.ObjectController.extend({
-  updateNeedStatus: function(key,value){
-      if(value === undefined){
-        return this.get("need");
-      }else {
 
-        if(!this.get("need")){
-          this.set('amount',1);
-        }
-        else{
-          this.set('amount', 0);
-        }
-
-        this.set("need",value);
-        this.get('model').save();
-        return this.get("need");
-      }
-  }.property('model.updateNeedStatus'),
 
   actions: {
     more: function() {

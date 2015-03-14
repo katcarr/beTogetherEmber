13e624446758905857afa16da600cf9ea549e9b3
katcarr <kathrynelizabethcarr@gmail.com>
2015-03-13 21:02:42 -0700

    Change the url for trip template

--- router.js
+++ router.js
@@ -1,6 +1,6 @@
 BeTogether.Router.map(function(){
   this.resource('home', {path: '/'});
   this.resource('profile', function(){
-    this.resource('trip', {path: ':trip_id'});
+    this.resource('trip', {path: 'trips/:name'});
   });
 });
